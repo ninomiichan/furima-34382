9404bebddddf1475c0875d2b5b369621a93a7d25
ninomiichan <michan_b_u@yahoo.co.jp>
2021-03-22 20:00:32 +0900

    ログイン機能訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   private
 
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day])
   end
 
   def basic_auth
