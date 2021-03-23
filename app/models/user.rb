88233dd10e17b3a9425ef7b4ce010d59562b82e1
ninomiichan <michan_b_u@yahoo.co.jp>
2021-03-23 17:56:28 +0900

    テストコード訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,38 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
+         
+  with_options presence: true do
+    validates :nickname
+    validates :family_namelength: {with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,}
+    validates :first_namelength: {with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,}
+    validates :family_name_kana,length: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,}
+    validates :first_name_kana,length: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,}
+    validates :birth_day
+  end
+
+  validates :password,length: { /\A[a-z0-9]+\z/i
+  validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+    
   
-       
 end
+
+
+  # validates :nickname, with_optionsformat: {
+  #   with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+  #   message: "は、全角で入力して下さい"
+  #   }
+  # validates :family_name, with_optionsformat: {
+  #   with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+  #   message: "は、全角で入力して下さい"
+  #   }
+  # validates :first_name, with_optionsformat: {
+  #   with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+  #   message: "は、全角で入力して下さい"
+  #   }
+  # validates :family_name_kana, with_options {
+  #   with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+  #   message: 'は全角カナで入力して下さい。'
+  # validates :first_name_kana, with_options {
+  #   with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+  #   message: 'は全角カナで入力して下さい。'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,10 +51,59 @@
             @user.valid?
             expect(@user.errors.full_messages).to include "Birth day can't be blank"
           end
+          it " passwordは全角では登録できないこと " do
+            @user.password = "テスト"
+            @user.valid?
+            expect(user.errors[:password]).to include("")
+          end
+          it " passwordは英語のみでは登録できないこと" do
+            @user.password=  "テスト"
+            @user.valid?
+            expect(user.errors[:password]).to include("")
+          end
+          it " emailは＠を含むこと " do
+            @user.email =  "test"
+            @user.valid?
+            expect(user.errors[:password]).to include("")
+          end
+          it " emailは一意性であること " do
+            @user.email =  "00000"
+            expect(user.errors[:password]).to include("")
+          end
+          it " family_nameは漢字・平仮名・カタカナ以外では登録できないこと " do
+            @user.family_name =  "test"
+            @user.valid?
+            expect(user.errors[:password]).to include("")
+          end
+          it " first_nameは漢字・平仮名・カタカナ以外では登録できないこと " do
+            @user.first_name =  "test"
+            @user.valid?
+            expect(user.errors[:password]).to include("")
+          end
+          it " first_name_kanaは全角カタカナ以外では登録できないこと " do
+            @user.first_name_kana = "test"
+            @user.valid?
+            expect(user.errors[:password]).to include("")
+          end
+          it " family_name_kanaは全角カタカナ以外では登録できないこと " do
+            @user.family_name_kana = 'test'
+            @user.valid?
+            expect(@user.errors.full_messages).to include("")
+          end
+          it " passwordは数字のみでは登録できないこと" do
+            @user.password= build(:user, password: "00000", password_confirmation: "00000")
+            @user.valid?
+            expect(@user.errors.full_messages).to include("")
+          end
+          it " passwordが5文字以下であれば登録できないこと " do
+            @user.password= build(:user, password: "00000", password_confirmation: "00000")
+            @user.valid?
+            expect(user.errors[:password]).to include("Is too short (minimum is 6 characters)")
+          end
       end
     end
 
-   
+
    
 
     
