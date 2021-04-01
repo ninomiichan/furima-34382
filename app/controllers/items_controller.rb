7fa52ccf64168b75bfb560f7c4be197e5111f7e0
ninomiichan <michan_b_u@yahoo.co.jp>
2021-04-01 20:07:10 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,22 @@
 class ItemsController < ApplicationController
-  
-  
+
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show]
   def index
-    
-    
+    @items = Item.includes(:image).order('created_at DESC')
   end
+
+  def new
+    @item = Item.new
+    @item.images.build
+    @category_parent_array = ["---"]
+    @category_parent_array = Category.where(ancestry: nil)
   end
+
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    @category_children = Category.find(params[:parent_id]).children
+  end
+end
+end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def credit_register
+  end
+
+  def logout
+    @user = current_user
+  end
 end
