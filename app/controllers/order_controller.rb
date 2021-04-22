993e26b9be8de8193d95e5bb9cf3895a14cac976
ninomiichan <michan_b_u@yahoo.co.jp>
2021-04-22 22:46:54 +0900

    決算機能

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -9,8 +9,15 @@ class OrderController < ApplicationController
   end
 
   def create
+    binding.pry
     @order_address = OrderAddress.new(order_params)
     if  @order_address.valid?
+      Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -18,10 +25,19 @@ class OrderController < ApplicationController
     end
   end
 
-  private
+private
 
-  def order_params
-    params.require(:order_address).permit(:post_code, :prefecture, :city, :address, :building_name,:phone_number,:order).merge(user_id: current_user.id)
-  end
+def order_params
+  params.require(:order).permit(:price).merge(token: params[:token])
+end
+
+def pay_item
+  Payjp.api_key = "sk_test_ab1bafde797142cd9c2b55ee"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+  Payjp::Charge.create(
+    amount: order_params[:price],  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+end
 
 end
