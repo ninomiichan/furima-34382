46ab3725ed9ca998a65b14ea0c54133a5aea9020
ninomiichan <michan_b_u@yahoo.co.jp>
2021-04-22 12:50:59 +0900

    修正８

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
 
   def update
     @item.update(item_params)
-    if @item.save
+    if @item.update
       redirect_to item_path
     else
       render :edit
