73898371cec23b134ec0131fd910479d42783aac
ninomiichan <michan_b_u@yahoo.co.jp>
2021-04-22 18:43:11 +0900

    コントローラー作成

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,27 @@
 class OrderController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+  end
+
+  def new
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if  @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture, :city, :address, :building_name,:phone_number,:order).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/orderaddress.rb
+++ app/models/orderaddress.rb
@@ -11,7 +11,7 @@ class OrderAddress
   def save
       order = Order.create(user: user, item: item)
      
-      Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number,order: order)
+      Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number,order:)
     end
   end
 end
