214ea6862c96ef8e3989dfbac0df7900d34be2a9
ninomiichan <michan_b_u@yahoo.co.jp>
2021-04-22 17:18:06 +0900

    購入機能実装

---
+++ db/migrate/20210422080721_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.references :user,   null: false, foreign_kye:true
+      t.references :item,   null: false, foreign_kye:true
+      
+      t.timestamps
+    end
+  end
+end
