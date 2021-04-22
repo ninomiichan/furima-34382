d4d983e0f664ce1373d471b5e0de642c49c50aa2
ninomiichan <michan_b_u@yahoo.co.jp>
2021-04-22 11:41:49 +0900

    修正６

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-
-  before_action :move_to_index, except: [:index, :show,]  
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show,]  
+  
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -24,29 +24,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
     
-  def edit
-    @item = Item.find(params[:id])
+  
+    def edit
+      @item = Item.find(params[:id])
+      if  unless current_user.id == @item.user.id
+        redirect_to root_path
+      end
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      @item.update(item_params)
+      if @item.save
+        redirect_to root_path
+       else
+         render :new
+      end
+    end
   end
 
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to root_path
-     else
-       render :new
-     end
+  private
+  def item_params
+    params.require(:item).permit(:image, :category_id, :itemtext, :title, :condition_id, :postage_id, :prefecture_id, :shipping_day_id,:price).merge(user_id: current_user.id)
   end
-
-
-private
-def item_params
-  params.require(:item).permit(:image, :category_id, :itemtext, :title, :condition_id, :postage_id, :prefecture_id, :shipping_day_id,:price).merge(user_id: current_user.id)
-end
-
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
-end
 end
